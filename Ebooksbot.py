87436c96b0193bbd085b110987556374ef4d977c
Prashanth Vasudevan <prashathvsdvn@gmail.com>
2020-04-13 17:39:16 +0530

    Post & Comment scraping and getting URL is working

--- Ebooksbot.py
+++ Ebooksbot.py
@@ -1,27 +1,32 @@
 import praw
+import os
 
 #Reddit API login
 
-reddit = praw.Reddit(client_id='',
-                     client_secret='',
-                     username='prashanthvsdvn'
-                     password=''
-                     user_agent='')
+reddit = praw.Reddit(client_id='6_4z5ED51KUb8g', client_secret='LUxQnirGsYm4iZuxfpRe6_9qLAg', user_agent='Ebook client v0.1')
 
 #Active Sub reddit
 subreddit = reddit.subreddit('FreeEBOOKS')
 
+#Create a txt file
+f=open("books.txt","at")
+
+#function to scrape the link
 def getURL(comment):
-    lines = comment.readlines()
-    for line in lines:
-        if line.find('amazon.in'):
-            getbook(line)
+    commentlines = comment.splitlines()
+    for line in commentlines:
+        if (line.find('amazon.in') == 1):#Searching for amazon.in links
+            f.write(line[12:-2]+"\n")
+            os.system("firefox " + line[12:-2])#Opening all the links with firefox
 
-def getbook(line):
+#Searching through the subreddit for new posts
+for post in subreddit.new():
+    #Checking for books which are tagged as expired
+    if (post.link_flair_text!= "Expired"):
+        for comment in post.comments:
+            #Checking for comment by a link sharing bot
+            if (comment.author == "amazon-converter-bot"):
+                f.write("{} (flair:{})\n".format(post.title,post.link_flair_text))
+                getURL(comment.body)
 
-#Search for Amazon links
-for post in subreddit.stream.posts():
-    for comments in subreddit.stream.comments():
-        if comments.user == amazon-converter-bot:
-            getURL(comments.body)
-
+f.close()
